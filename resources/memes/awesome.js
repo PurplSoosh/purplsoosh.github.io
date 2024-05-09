8ed7c4511a7dab1aec843261fdea9ce73a95cbbe
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-09 20:35:57 +0900

    awesomer.js

--- resources/memes/awesome.js
+++ resources/memes/awesome.js
@@ -3,45 +3,48 @@ function balls(vid, title, id, info, offensive, epileptic) {
         info = "Info isn't available.";
     }
 
-    document.write(
-        "<!DOCTYPE html>"+
-        "<html>" +
-        "<head>" +
-        "<title>ID " + id + ": " + title + "</title>" +
-        "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>" +
-        "<meta property='og:video' content='" + vid + "'>" +
-	"<meta property=''content='video.movie'>" +
-        "</head>" +
-        "<body>" +
-        "<div class='container'>" +
-        "<div class='sectionTop'>" +
-        "<center>" +
-        "Shitpost Centre - " + title + "<br>" +
-	if (id < 0) {
-	    "<a href='https://purplsoosh.github.io/resources/memes/" + (id + 1) + ">Previous</a> --- <a href='../../../pages/shitpost/'>Reroll</a> --- <a href='https://purplsoosh.github.io/resources/memes/" + (id - 1) + ">Next</a>" +
-	} else {
-	"<a href='https://purplsoosh.github.io/resources/memes/" + (id - 1) + ">Previous</a> --- <a href='../../../pages/shitpost/'>Reroll</a> --- <a href='https://purplsoosh.github.io/resources/memes/" + (id + 1) + ">Next</a>" +
-		}
-        "</center>" +
-        "</div>" +
-        "<div class='sections'>" +
-        "<div class='sectionLeft'>" +
-        "<center>" +
-        "<video width='640' height='480' controls=''>" +
-        "<source src='" + vid + "' type='video/mp4'>" +
-        "GET A BETTER BROWSER OH MY GOD" +
-        "</video></center>" +
-        "<small>" + info + "</small>" +
-        "</div>" +
-        "<div class='sectionRight'>" +
-        "<marquee scrollspeed='8'>Other pages!</marquee>" +
-        "<a href='../../../pages/changelog/'>Changelog</a><br>" +
-        "<a href='../../../pages/servers/'>Server list</a><br>" +
-        "<a href='../../../pages/shitpost/'>Shitpost Generator</a><br>" +
-        "<a href='../../../'>Main page</a>" +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        "</body>" +
-        "</html>");
+var htmlContent = "<!DOCTYPE html>"+
+    "<html>" +
+    "<head>" +
+    "<title>ID " + id + ": " + title + "</title>" +
+    "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>" +
+    "<meta property='og:video' content='" + vid + "'>" +
+    "<meta property=''content='video.movie'>" +
+    "</head>" +
+    "<body>" +
+    "<div class='container'>" +
+    "<div class='sectionTop'>" +
+    "<center>" +
+    "Shitpost Centre - " + title + "<br>";
+
+if (id < 0) {
+    htmlContent += "<a href='https://purplsoosh.github.io/resources/memes/" + (id + 1) + "'>Previous</a> --- <a href='../../../pages/shitpost/'>Reroll</a> --- <a href='https://purplsoosh.github.io/resources/memes/" + (id - 1) + "'>Next</a>";
+} else {
+    htmlContent += "<a href='https://purplsoosh.github.io/resources/memes/" + (id - 1) + "'>Previous</a> --- <a href='../../../pages/shitpost/'>Reroll</a> --- <a href='https://purplsoosh.github.io/resources/memes/" + (id + 1) + "'>Next</a>";
 }
+
+htmlContent +=
+    "</center>" +
+    "</div>" +
+    "<div class='sections'>" +
+    "<div class='sectionLeft'>" +
+    "<center>" +
+    "<video width='640' height='480' controls=''>" +
+    "<source src='" + vid + "' type='video/mp4'>" +
+    "GET A BETTER BROWSER OH MY GOD" +
+    "</video></center>" +
+    "<small>" + info + "</small>" +
+    "</div>" +
+    "<div class='sectionRight'>" +
+    "<marquee scrollspeed='8'>Other pages!</marquee>" +
+    "<a href='../../../pages/changelog/'>Changelog</a><br>" +
+    "<a href='../../../pages/servers/'>Server list</a><br>" +
+    "<a href='../../../pages/shitpost/'>Shitpost Generator</a><br>" +
+    "<a href='../../../'>Main page</a>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</body>" +
+    "</html>";
+
+document.write(htmlContent);
