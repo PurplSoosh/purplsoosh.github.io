e00fea771e889da82546285b4d2549a90644842d
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-09 18:31:47 +0900

    Goofy

--- resources/memes/awesome.js
+++ resources/memes/awesome.js
@@ -4,6 +4,15 @@ function balls(vid, title, id, info) {
     }
 
     document.write(+
+        "<!DOCTYPE html>"+
+        "<html>" +
+        "<head>" +
+        "<title>ID " + id + ": " + title + "</title>" +
+        "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>" +
+        "<meta property='og:video' content='" + vid + "'>" +
+	"<meta property=''content='video.movie'>" +
+        "</head>" +
+        "<body>" +
         "<div class='container'>" +
         "<div class='sectionTop'>" +
         "<center>" +
@@ -27,5 +36,7 @@ function balls(vid, title, id, info) {
         "<a href='../../../'>Main page</a>" +
         "</div>" +
         "</div>" +
-        "</div>");
+        "</div>" +
+        "</body>" +
+        "</html>");
 }
