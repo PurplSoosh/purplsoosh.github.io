d8e2e7c79cefa18fe3595d6480f8b64e55874147
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-09 17:34:31 +0900

    AWESOME OPOSSUM!!!!!

--- resources/memes/awesome.js
+++ resources/memes/awesome.js
@@ -1,41 +1,41 @@
 function balls(vid, title, id, info) {
-  if (info === null) {
-    info = "";
-  };
-document.write("<!DOCTYPE html> \
-<html> \
-<head>\
-               <title>ID "+id+": "+title+"</title>\
-<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'> \
-<meta property='og:video' content='"+vid+"'> \
-<meta property='og:type' content='video.movie'>\
-</head>\
-<body>\
-  <div class='container'>\
-    <div class='sectionTop'>\
-      <center>\
-        Shitpost Centre - "+title+"\
-      </center>\
-  </div>\
-    <div class=\"sections\">\
-      <div class=\"sectionLeft\">\
-            <center>\
-          <video width=\"640\" height=\"480\" controls=\"\">\
-  <source src="+vid+" type=\"video/mp4\">\
-GET A BETTER BROWSER OH MY GOD\
-                </video></center>\
- \
-               <small>"+info+"</small>\
-      </div>\
-      <div class=\"sectionRight\">\
-            <marquee scrollspeed='8'>Other pages!</marquee>\
-       <a href='../../../pages/changelog/'>Changelog</a><br>\
-       <a href='../../../pages/servers/'>Server list</a><br>\
-       <a href='../../../pages/shitpost/'>Shitpost Generator</a><br>\
-       <a href='../../../'>Main page</a>\
-      </div>\
-    </div>\
-  </div>\
-</html>\
-")
+    if (info === null || info === undefined) {
+        info = "Info isn't available.";
+    }
+
+    document.write("<!DOCTYPE html>" +
+        "<html>" +
+        "<head>" +
+        "<title>ID " + id + ": " + title + "</title>" +
+        "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>" +
+        "<meta property='og:video' content='" + vid + "'>" +
+        "<meta property='og:type' content='video.movie'>" +
+        "</head>" +
+        "<body>" +
+        "<div class='container'>" +
+        "<div class='sectionTop'>" +
+        "<center>" +
+        "Shitpost Centre - " + title +
+        "</center>" +
+        "</div>" +
+        "<div class='sections'>" +
+        "<div class='sectionLeft'>" +
+        "<center>" +
+        "<video width='640' height='480' controls=''>" +
+        "<source src='" + vid + "' type='video/mp4'>" +
+        "GET A BETTER BROWSER OH MY GOD" +
+        "</video></center>" +
+        "<small>" + info + "</small>" +
+        "</div>" +
+        "<div class='sectionRight'>" +
+        "<marquee scrollspeed='8'>Other pages!</marquee>" +
+        "<a href='../../../pages/changelog/'>Changelog</a><br>" +
+        "<a href='../../../pages/servers/'>Server list</a><br>" +
+        "<a href='../../../pages/shitpost/'>Shitpost Generator</a><br>" +
+        "<a href='../../../'>Main page</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</body>" +
+        "</html>");
 }
