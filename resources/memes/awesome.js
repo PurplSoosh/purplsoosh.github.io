614586f59b2bc1c2d16124669ee24614d960f6ae
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-09 17:05:39 +0900

    awesome js

---
+++ resources/memes/awesome.js
@@ -0,0 +1,35 @@
+function balls(var, title, id) {
+document.write("<!DOCTYPE html> \
+<html> \
+<head>\
+<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'> \
+<meta property='og:video' content='"+vid+"'> \
+</head>\
+<body>\
+  <div class='container'>\
+    <div class='sectionTop'>\
+      <center>\
+        Shitpost Centre - "+title+"\
+      </center>\
+  </div>\
+    <div class=\"sections\">\
+      <div class=\"sectionLeft\">\
+            <center>\
+          <video width=\"640\" height=\"480\" controls=\"\">\
+  <source src="+vid+" type=\"video/mp4\">\
+GET A BETTER BROWSER OH MY GOD\
+                </video></center>\
+ \
+      </div>\
+      <div class=\"sectionRight\">\
+            <marquee scrollspeed='8'>Other pages!</marquee>\
+       <a href='../../../pages/changelog/'>Changelog</a><br>\
+       <a href='../../../pages/servers/'>Server list</a><br>\
+       <a href='../../../pages/shitpost/'>Shitpost Generator</a><br>\
+       <a href='../../../'>Main page</a>\
+      </div>\
+    </div>\
+  </div>\
+</html>\
+")
+}
