a7d158e692ff6b624459b4db184a7cc318b81e42
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-12 16:11:33 +0800

    balls

--- resources/memes/awesome.js
+++ resources/memes/awesome.js
@@ -56,7 +56,7 @@ htmlContent += "</center>" +
     "GET A BETTER BROWSER OH MY GOD" +
     "</video>"
     } else {
-        htmlContent += '<img src="https://purplsoosh.github.io/resources/memes/1/' + vid + '" alt=' + title + ' width="640" height="480">'
+        htmlContent += '<img width="640" height="640" src="https://purplsoosh.github.io/resources/memes/1/' + vid + '" alt=' + title + '>'
     }
     htmlContent += "</div></center>" +
     "<small>" + info + "</small>" +
