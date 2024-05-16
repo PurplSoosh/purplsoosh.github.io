7ece3a99e8359c776a72e7558469ce234401d07e
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-16 19:21:54 +0800

    スキビディトイレト

--- resources/memes/awesome.js
+++ resources/memes/awesome.js
@@ -13,8 +13,8 @@ var htmlContent = "<!DOCTYPE html>"+
     "<html>" +
     "<head>" +
     "<title>ID " + id + ": " + title + "</title>" +
-    "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>";
-    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
+    "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>" +
+    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
     if (id <= 0) {
     htmlContent += "<meta property='og:video' content='" + vid + "'>" +
     "<meta property=''content='video.movie'>" };
