d0cc685ae99bff143b553adb876a66f4a339fa66
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-16 18:53:01 +0800

    wonderhoy

---
+++ resources/memes/moreshit.js
@@ -0,0 +1,12 @@
+function hi() {
+    print("Oh oh ohio!");
+    print("Welcome to my Skibidi Toilet");
+    print("Oh oh ohio!");
+    print("Please don't rizz, I have no gyatts");
+    print("Oh oh ohio!");
+    print("Let's be sigma, lemme rizz ya!");
+    print("Oh oh ohio!");
+    print("Haha! I prayed today!");
+};
+
+// Add more shit here when I need to
