272d66bd9deb77857757f3ee285a836275096845
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-16 18:57:39 +0800

    あなたゲイじゃないだ？
    only japanese i know fr

--- resources/memes/awesome.js
+++ resources/memes/awesome.js
@@ -14,7 +14,7 @@ var htmlContent = "<!DOCTYPE html>"+
     "<head>" +
     "<title>ID " + id + ": " + title + "</title>" +
     "<link rel='stylesheet' href='https://purplsoosh.github.io/resources/main.css'>";
-
+    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
     if (id <= 0) {
     htmlContent += "<meta property='og:video' content='" + vid + "'>" +
     "<meta property=''content='video.movie'>" };
