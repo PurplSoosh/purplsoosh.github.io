f92e78810b99f1e5c64e830af368b3f7bf90213e
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-17 17:35:59 +0800

    JS.

---
+++ resources/main.js
@@ -0,0 +1,17 @@
+// Define the custom element class for dprc-center
+class DPRCCenterElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+// Define the custom element class for dprc-marquee
+class DPRCMarqueeElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+// Register the custom elements
+customElements.define('dprc-center', DPRCCenterElement);
+customElements.define('dprc-marquee', DPRCMarqueeElement);
