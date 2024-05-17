e2b28d8c2b5728e0eccbe72ec8439589570f711f
PurplSoosh <168150169+PurplSoosh@users.noreply.github.com>
2024-05-17 17:40:53 +0800

    acoustic ahh

--- resources/main.js
+++ resources/main.js
@@ -1,21 +1,4 @@
-// Define the custom element class for dprc-center
-class DPRCCenterElement extends HTMLElement {
-  constructor() {
-    super();
-  }
-}
-
-// Define the custom element class for dprc-marquee
-class DPRCMarqueeElement extends HTMLElement {
-  constructor() {
-    super();
-  }
-}
-
-// Register the custom elements
-customElements.define('dprc-center', DPRCCenterElement);
-customElements.define('dprc-marquee', DPRCMarqueeElement);
-
+// im acoustic ahhh
 window.onload = function() {
   document.getElementById("needJS").innerHTML = "";
 };
